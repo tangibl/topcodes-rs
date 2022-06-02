d84bd05894e2e0862a933176171df4ccd7589e30
Byron Batteson <byronbatteson@gmail.com>
2022-06-02 12:44:36 +0200

    Add bigger source photo to benches
    The recent changes showed that performance for the photo decreased while
    performance for the larger source image improved.

--- benches/scanner.rs
+++ benches/scanner.rs
@@ -23,6 +23,22 @@ fn criterion_benchmark(c: &mut Criterion) {
             criterion::BatchSize::SmallInput,
         )
     });
+
+    c.bench_function("Scanner (source)", |b| {
+        b.iter_batched(
+            || {
+                let img = ImageReader::open("assets/source.png")
+                    .unwrap()
+                    .decode()
+                    .unwrap();
+                let (width, height) = (img.width() as usize, img.height() as usize);
+                let buffer = img.into_rgb8().into_raw();
+                (Scanner::new(width, height), buffer)
+            },
+            |(mut scanner, buffer)| scan(&mut scanner, &buffer),
+            criterion::BatchSize::SmallInput,
+        )
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
