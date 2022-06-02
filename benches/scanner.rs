189375e72d6fe6efe6a65caead14d2dd1b1e5c10
Byron Batteson <byronbatteson@gmail.com>
2022-06-02 09:35:49 +0200

    Add benchmark support
    This uses Criterion.rs instead of the default libtest, as libtest
    currently relies on nightly. Criterion also has some nice reporting
    features.

---
+++ benches/scanner.rs
@@ -0,0 +1,25 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use image::io::Reader as ImageReader;
+use topcodes::scanner::Scanner;
+
+fn scan(buffer: &[u8], width: usize, height: usize) {
+    let mut scanner = Scanner::new(buffer, width, height);
+    let topcodes = scanner.scan();
+    assert_eq!(3, topcodes.len());
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let img = ImageReader::open("assets/photo.png")
+        .unwrap()
+        .decode()
+        .unwrap();
+    let (width, height) = (img.width() as usize, img.height() as usize);
+    let image_raw = img.into_rgb8().into_raw();
+    let buffer = &image_raw;
+    c.bench_function("scanner 12 samples", |b| {
+        b.iter(|| scan(buffer, width, height))
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
